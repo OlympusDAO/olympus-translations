230dcd76240d49c66b23c25e3a7578ff9b7bf477
ivelin <ivelin@ambianic.ai>
2022-03-08 23:13:53 +0000

    buid: deploy preview for translation PRs
    Signed-off-by: ivelin <ivelin@ambianic.ai>

---
+++ scripts/netlify-preview.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir frontend
+cd frontend
+git clone https://github.com/OlympusDAO/olympus-frontend
+rm -rf src/locales/translations/*
+mv olympus-frontend src/locales/translations/
+cd frontend
+yarn install --frozen-lockfile
+yarn build
