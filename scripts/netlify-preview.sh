7eff1ac25cdc9a2efa62716d7a816873df05cf8f
ivelin <ivelin@ambianic.ai>
2022-03-09 04:29:15 +0000

    chore: fix github action script
    Signed-off-by: ivelin <ivelin@ambianic.ai>

--- scripts/netlify-preview.sh
+++ scripts/netlify-preview.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-mkdir frontend
+pwd
+ls -al
+echo "Checkout front end default(develop) branch"
+git clone https://github.com/OlympusDAO/olympus-frontend frontend
+echo "Remove translations from frontend develop checkout"
+rm -rf frontend/src/locales/translations/*
+echo "Move current branch translations into place for build"
+mv olympus-frontend frontend/src/locales/translations/
+echo "Switch to frontend work dir"
 cd frontend
-git clone https://github.com/OlympusDAO/olympus-frontend
-rm -rf src/locales/translations/*
-mv olympus-frontend src/locales/translations/
-cd frontend
+pwd
+ls -al
+echo "Build OlympusDAP frontent web site"
 yarn install --frozen-lockfile
 yarn build
 pwd
