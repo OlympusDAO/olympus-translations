3a8d3a198bb408c4ccd7c6f035c2b9a8793d6cb0
ivelin <ivelin@ambianic.ai>
2022-03-09 05:30:48 +0000

    chore: testing CI preview
    Signed-off-by: ivelin <ivelin@ambianic.ai>

--- scripts/netlify-preview.sh
+++ scripts/netlify-preview.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 # enable explicit bash output
 set -x
-pwd
-ls -al
 echo "Checkout front end default(develop) branch"
 git clone https://github.com/OlympusDAO/olympus-frontend frontend
 echo "Remove translations from frontend develop checkout"
 rm -rf frontend/src/locales/translations/*
 echo "Move current branch translations into place for build"
-ls -al olympus-frontend
 cp -rf olympus-frontend frontend/src/locales/translations/
-ls -al frontend/src/locales/translations/
-ls -al frontend/src/locales/translations/olympus-frontend
 # prevent submodule pull
 touch frontend/src/locales/translations/.git
-ls -al frontend/src/locales/translations/
 echo "Switch to frontend work dir"
 cd frontend
-pwd
-ls -al
 echo "Prevent build from failing on warnings"
 export CI=false
 echo "Build OlympusDAP frontent web site "
 yarn install --frozen-lockfile
 yarn build
-pwd
-ls -al
